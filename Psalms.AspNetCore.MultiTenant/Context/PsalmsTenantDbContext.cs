30f57ac3e882f13cf3079c5605e1247d2fefdefe
Gustavo Soares <sirgustavo4@gmail.com>
2025-08-25 18:31:04 -0300

    feat: Criando Context

---
+++ Psalms.AspNetCore.MultiTenant/Context/PsalmsTenantDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Psalms.AspNetCore.MultiTenant.Models;
+
+namespace Psalms.AspNetCore.MultiTenant.Context;
+
+public class PsalmsTenantDbContext(DbContextOptions<PsalmsTenantDbContext> options) : DbContext(options)
+{
+    public DbSet<TenantModel> Tenants { get; set; }
+}
