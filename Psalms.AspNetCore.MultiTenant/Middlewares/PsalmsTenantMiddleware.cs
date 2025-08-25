df014eacb9b99271b0e0c15aa0b50a045ff9669b
Gustavo Soares <sirgustavo4@gmail.com>
2025-08-25 18:30:16 -0300

    feat: Criando Middleware

---
+++ Psalms.AspNetCore.MultiTenant/Middlewares/PsalmsTenantMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Psalms.AspNetCore.MultiTenant.Context;
+using Psalms.AspNetCore.MultiTenant.Enums;
+
+namespace Psalms.AspNetCore.MultiTenant.Middlewares;
+
+public class PsalmsTenantMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    { 
+        var tenantIdClaim = context.User?.FindFirst(TenantInfo.TenantId.ToString())?.Value;
+
+        if (string.IsNullOrEmpty(tenantIdClaim) || !int.TryParse(tenantIdClaim, out var tenantId))
+        {
+            await next(context);
+            return;
+        }
+
+        using var scope = context.RequestServices.CreateScope();
+        var tenantDb    = scope.ServiceProvider.GetRequiredService<PsalmsTenantDbContext>();
+        var memory      = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+
+        var dbName = (await tenantDb.Tenants.AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id == int.Parse(tenantIdClaim)))?.DatabaseName;
+
+        if (dbName == null)
+        {
+            context.Response.StatusCode = 403;
+            await context.Response.WriteAsync("Tenant id not founded.");
+            return;
+        }
+
+        memory.Set(TenantInfo.DatabaseName, dbName);
+
+        await next(context);
+    }
+}
