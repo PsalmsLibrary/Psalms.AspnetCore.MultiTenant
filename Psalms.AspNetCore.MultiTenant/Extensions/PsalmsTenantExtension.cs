64942fca5d472079b00d41698f4cbb5d764204ee
Gustavo Soares <sirgustavo4@gmail.com>
2025-08-25 18:30:27 -0300

    feat: Criando Extension

---
+++ Psalms.AspNetCore.MultiTenant/Extensions/PsalmsTenantExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Psalms.AspNetCore.MultiTenant.Context;
+using Psalms.AspNetCore.MultiTenant.Services;
+
+namespace Psalms.AspNetCore.MultiTenant.Extensions;
+
+public static class PsalmsTenantExtension
+{
+    public static IServiceCollection AddPsalmsMultiTenant(this IServiceCollection service, Action<DbContextOptionsBuilder> options)
+    {
+        service.AddDbContext<PsalmsTenantDbContext>(options);
+        service.AddScoped<IPsalmsTenantService, PsalmsTenantService>();
+        service.AddMemoryCache();
+        return service;
+    }
+}
