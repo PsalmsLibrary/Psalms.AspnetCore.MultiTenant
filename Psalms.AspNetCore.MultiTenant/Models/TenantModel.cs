241e386642d18325e1ff3b368efbc096f999fd8a
Gustavo Soares <sirgustavo4@gmail.com>
2025-08-25 18:30:43 -0300

    feat: Criando TenantModel

---
+++ Psalms.AspNetCore.MultiTenant/Models/TenantModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Psalms.AspNetCore.MultiTenant.Models;
+
+public class TenantModel
+{
+    public int Id { get; set; } 
+    public string Name { get; set; } = string.Empty;
+    public string Subdomain { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
+}
