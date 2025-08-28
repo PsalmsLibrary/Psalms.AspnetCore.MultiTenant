48232d9801a45e8d4f3ba635496756c5bfb594e9
Gustavo Soares <sirgustavo4@gmail.com>
2025-08-28 20:20:36 -0300

    feat: Criando a interface ITenantModelBase

---
+++ Psalms.AspNetCore.MultiTenant/Models/ITenantModelBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Psalms.AspNetCore.MultiTenant.Models;
+
+public interface ITenantModelBase
+{
+    int Id              { get; set; } 
+    string Name         { get; set; }
+    string Subdomain    { get; set; }
+    string DatabaseName { get; set; }
+}
