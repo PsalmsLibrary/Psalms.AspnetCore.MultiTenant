6bf8308b8592195d051e440731745832d19757aa
Gustavo Soares <sirgustavo4@gmail.com>
2025-08-28 20:23:18 -0300

    feat: Criando a interface IPsalmsTenantDbContext<TTenant>

---
+++ Psalms.AspNetCore.MultiTenant/Context/IPsalmsTenantDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Psalms.AspNetCore.MultiTenant.Models;
+
+namespace Psalms.AspNetCore.MultiTenant.Context;
+
+public interface IPsalmsTenantDbContext<TTenant> where TTenant : class, ITenantModelBase
+{
+    public DbSet<TTenant> Tenants { get; set; }
+
+    Task ApplyChangesAsync();
+}
