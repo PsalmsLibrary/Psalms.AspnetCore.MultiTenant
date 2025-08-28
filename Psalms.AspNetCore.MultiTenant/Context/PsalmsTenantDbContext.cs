54a2609223f9c40c9338edee596c078313365dda
Gustavo Soares <sirgustavo4@gmail.com>
2025-08-28 20:21:02 -0300

    trash: Arquivos que sofreram exclusão para melhorias

--- Psalms.AspNetCore.MultiTenant/Context/PsalmsTenantDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Psalms.AspNetCore.MultiTenant.Models;
-
-namespace Psalms.AspNetCore.MultiTenant.Context;
-
-public class PsalmsTenantDbContext(DbContextOptions<PsalmsTenantDbContext> options) : DbContext(options)
-{
-    public DbSet<TenantModel> Tenants { get; set; }
-}
--- Psalms.AspNetCore.MultiTenant/Models/TenantModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Psalms.AspNetCore.MultiTenant.Models;
-
-public class TenantModel
-{
-    public int Id { get; set; } 
-    public string Name { get; set; } = string.Empty;
-    public string Subdomain { get; set; } = string.Empty;
-    public string DatabaseName { get; set; } = string.Empty;
-}
