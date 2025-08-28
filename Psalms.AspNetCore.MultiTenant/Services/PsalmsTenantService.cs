1b2c27b5a85ffb611b6cc3a3926efb4c75f077a0
Gustavo Soares <sirgustavo4@gmail.com>
2025-08-28 20:19:34 -0300

    refactor(PsalmsTenantService): Deixando a classe com o tenant genérico

--- Psalms.AspNetCore.MultiTenant/Services/PsalmsTenantService.cs
+++ Psalms.AspNetCore.MultiTenant/Services/PsalmsTenantService.cs
@@ -1,35 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Psalms.AspNetCore.MultiTenant.Context;
-using Psalms.AspNetCore.MultiTenant.DTOs;
 using Psalms.AspNetCore.MultiTenant.Models;
-using Psalms.EFCore.DataManager.UnitOfWork;
 using System.Linq.Expressions;
 
 namespace Psalms.AspNetCore.MultiTenant.Services;
 
-public class PsalmsTenantService(PsalmsTenantDbContext tenantContext, IConfiguration config) : IPsalmsTenantService
-{
-    #region UnitOfWork
-    private readonly Lazy<IPsalmsUnitOfWork<PsalmsTenantDbContext>> _psalmsUnitOfWork 
-        = new(() => new PsalmsUnitOfWork<PsalmsTenantDbContext>(tenantContext));
-    #endregion
-
+public class PsalmsTenantService<Tenant>(IPsalmsTenantDbContext<Tenant> tenantContext, IConfiguration config, MultiTenantConfigureDbContext Appcontext)
+    where Tenant : class, ITenantModelBase 
+{   
     #region IPsalmsTenantService Methods
-    public async Task<TenantModel?> GetTenantByAsync(Expression<Func<TenantModel, bool>> predicate)
-        => await _psalmsUnitOfWork.Value.Repository.GetByAsync(predicate);
+    public async Task<Tenant?> GetTenantByAsync(Expression<Func<Tenant, bool>> predicate)
+        => await tenantContext.Tenants.FirstOrDefaultAsync(predicate);
 
-    public async Task CreateTenantAsync(TenantDto tenant, DbContext context)
+    public async Task CreateTenantAsync(Tenant tenant)
     {
-        await _psalmsUnitOfWork.Value.Repository.CreateAsync(tenant.Map());
-        await _psalmsUnitOfWork.Value.SaveChangesAsync();
+        await tenantContext.Tenants.AddAsync(tenant);
+        await tenantContext.ApplyChangesAsync();
 
-        var connectionString = GetDbConnectionString(config, tenant.DatabaseName);
-        
-        context.Database.SetConnectionString(connectionString);
-        await context.Database.MigrateAsync();
+        var connectionString = PsalmsDatabase.GetDbConnectionString(config, tenant.DatabaseName);
+
+        Appcontext.Database.SetConnectionString(connectionString);
+        await Appcontext.Database.MigrateAsync();
     }
-    public static string GetDbConnectionString(IConfiguration config, string databaseName) => string.Format(config["DbConnectionBase"] ??
-            throw new Exception("Multi Tenant connection not found"), databaseName);
     #endregion
 }
