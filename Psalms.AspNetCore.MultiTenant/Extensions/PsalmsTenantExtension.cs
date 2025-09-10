8035fbb64e612ae47ab3fc2acbe8678f38a355ce
Gustavo Soares <sirgustavo4@gmail.com>
2025-09-10 20:27:16 -0300

    refactor: Ajustando generics nas classes

--- Psalms.AspNetCore.MultiTenant/Extensions/PsalmsTenantExtension.cs
+++ Psalms.AspNetCore.MultiTenant/Extensions/PsalmsTenantExtension.cs
@@ -11,14 +11,14 @@ public static class PsalmsTenantExtension
 {
     public static IServiceCollection AddPsalmsMultiTenant<TenantModel, TenantContext, AppContext>(this IServiceCollection service,
         IPsalmsContextConfiguration contextConfiguration, IConfiguration configuration)
-        where TenantModel : class, ITenantModelBase
-        where AppContext : MultiTenantConfigureDbContext
+        where TenantModel   : class, ITenantModelBase
+        where AppContext    : DbContext
         where TenantContext : DbContext, IPsalmsTenantDbContext<TenantModel>
     {
         service.AddDbContext<IPsalmsTenantDbContext<TenantModel>, TenantContext>(contextConfiguration.TenantContextConfig(configuration));
-        service.AddDbContext<MultiTenantConfigureDbContext, AppContext>(contextConfiguration.AppContextConfig());
+        service.AddDbContext<AppContext>(contextConfiguration.AppContextConfig());
 
-        service.AddScoped<PsalmsTenantService<TenantModel>>();
+        service.AddScoped<PsalmsTenantService<TenantModel, AppContext>>();
 
         return service; 
     }
