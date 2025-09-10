b3100f2c6d760725b257ccd46213572cb7bd98a3
Gustavo Soares <sirgustavo4@gmail.com>
2025-09-10 20:27:32 -0300

    feat: Criando um TenantContext padrão

---
+++ Psalms.AspNetCore.MultiTenant/Context/PsalmsTenantContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Psalms.AspNetCore.MultiTenant.Models;
+
+namespace Psalms.AspNetCore.MultiTenant.Context;
+
+public class PsalmsTenantContext(DbContextOptions<PsalmsTenantContext> options) : DbContext(options), IPsalmsTenantDbContext<PsalmsTenantModel>
+{
+    public DbSet<PsalmsTenantModel> Tenants { get; set; }
+
+    public Task ApplyChangesAsync() => SaveChangesAsync();
+}
