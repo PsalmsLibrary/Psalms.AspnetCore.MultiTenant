b0d988fec128b316c036634bdae5e3082917656b
Gustavo Soares <sirgustavo4@gmail.com>
2025-09-11 11:13:44 -0300

    feat: Criando um Dto para PsalmsTenantModel

--- Psalms.AspNetCore.MultiTenant/Context/PsalmsTenantContext.cs
+++ Psalms.AspNetCore.MultiTenant/Context/PsalmsTenantContext.cs
@@ -3,6 +3,10 @@ using Psalms.AspNetCore.MultiTenant.Models;
 
 namespace Psalms.AspNetCore.MultiTenant.Context;
 
+/// <summary>
+/// A default Tenant Context to a easily configuration.
+/// </summary>
+/// <param name="options"></param>
 public class PsalmsTenantContext(DbContextOptions<PsalmsTenantContext> options) : DbContext(options), IPsalmsTenantDbContext<PsalmsTenantModel>
 {
     public DbSet<PsalmsTenantModel> Tenants { get; set; }
---
+++ Psalms.AspNetCore.MultiTenant/Models/Dto/PsalmsTenantDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Psalms.AspNetCore.MultiTenant.Models.Dto;
+
+/// <summary>
+/// A default DTO to PsalmsTenant
+/// </summary>
+public class PsalmsTenantDto
+{
+    [Required(ErrorMessage= "Tenant Name is required.")]
+    public string Name { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Tenant Subdomain is required.")]
+    public string Subdomain { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Tenant DatabaseName is required.")]
+    public string DatabaseName { get; set; } = string.Empty;
+
+    public PsalmsTenantModel GetModel()
+        => new()
+        {
+            Name         = this.Name,
+            Subdomain    = this.Subdomain,
+            DatabaseName = this.DatabaseName,
+        };
+}
